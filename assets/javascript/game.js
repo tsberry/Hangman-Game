0b5a6a9ff39e530b73f74cd62139593675f916c2
unknown <tberry6@gmail.com>
2018-03-14 12:00:36 -0700

    Reworked with game as an object.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,107 @@
-var numWins = 0;
-var currWord = "";
-var wordGuessed = [];
-var numGuesses = 8;
-var lettersGuessed = [];
-var guess = null;
-var words = ["gorilla", "turtle", "penguin", "chimpanzee", "elephant", "rhino"];
 
-function makeWord() {
-    var rand = Math.floor(Math.random() * (words.length));
-    currWord = words[rand];
-    wordGuessed = [];
-    for(var i = 0; i < currWord.length; i++) {
-        wordGuessed = wordGuessed.concat("_");
-    }
-}
 
-function guessLetter(letter) {
-    var added = false;
-    var found = false;
-    for(i = 0; i < currWord.length; i++) {
-        if (currWord.charAt(i) === letter) {
-            wordGuessed[i] = letter;
-            found = true;
+var game = {
+    numWins: 0,
+    currWord: "",
+    wordGuessed: [],
+    numGuesses: 8,
+    lettersGuessed: [],
+    guess: null,
+    words: ["gorilla", "turtle", "penguin", "chimpanzee", "elephant", "rhino"],
+
+    makeWord: function () {
+        var rand = Math.floor(Math.random() * (this.words.length));
+        this.currWord = this.words[rand];
+        this.wordGuessed = [];
+        for (var i = 0; i < this.currWord.length; i++) {
+            this.wordGuessed = this.wordGuessed.concat("_");
+        }
+    },
+
+    guessLetter: function (letter) {
+        var added = false;
+        var found = false;
+        for (i = 0; i < this.currWord.length; i++) {
+            if (this.currWord.charAt(i) === letter) {
+                this.wordGuessed[i] = letter;
+                found = true;
+            }
+        }
+        if (this.lettersGuessed.indexOf(letter) == -1) {
+            this.lettersGuessed = this.lettersGuessed.concat(letter);
+            added = true;
+        }
+        if (!found && added) {
+            this.numGuesses--;
+        }
+    },
+
+    printWins: function () {
+        document.getElementById("wins").innerHTML = "Number of wins: " + this.numWins;
+    },
+
+    printWord: function () {
+        var wordSpaced = "";
+        for (var i = 0; i < this.wordGuessed.length; i++) {
+            wordSpaced += this.wordGuessed[i] + "  ";
+        }
+        document.getElementById("word-spaces").innerHTML = wordSpaced;
+    },
+
+    printGuesses: function () {
+        document.getElementById("num-guesses").innerHTML = this.numGuesses;
+    },
+
+    printLetters: function () {
+        var lettersSpaced = "";
+        for (var i = 0; i < this.lettersGuessed.length; i++) {
+            lettersSpaced += this.lettersGuessed[i] + "  ";
+        }
+        lettersSpaced = lettersSpaced.toUpperCase();
+        document.getElementById("letters-guessed").innerHTML = lettersSpaced;
+    },
+
+
+
+    start: function () {
+        this.makeWord();
+        this.numGuesses = 8;
+        this.lettersGuessed = [];
+        this.printWins();
+        this.printWord();
+        this.printGuesses();
+        this.printLetters();
+    },
+
+    update: function (guess) {
+        this.guessLetter(guess);
+        this.printWins();
+        this.printWord();
+        this.printGuesses();
+        this.printLetters();
+        if (equals(this.currWord.split(""), this.wordGuessed)) {
+            alert("You win!");
+            this.numWins += 1;
+            this.start();
+        }
+        if (this.numGuesses === 0) {
+            alert("You Lose!");
+            this.start();
         }
     }
-    if(lettersGuessed.indexOf(letter) == -1) {
-        lettersGuessed = lettersGuessed.concat(letter);
-        added = true;
-    }
-    if(!found && added) {
-        numGuesses--;
-    }
-}
-
-function printWins() {
-    document.getElementById("wins").innerHTML = "Number of wins: " + numWins;
-}
-
-function printWord() {
-    var wordSpaced = "";
-    for(var i = 0; i < wordGuessed.length; i++) {
-        wordSpaced += wordGuessed[i] + "  ";
-    }
-    document.getElementById("word-spaces").innerHTML = wordSpaced;
-}
-
-function printGuesses() {
-    document.getElementById("num-guesses").innerHTML = numGuesses;
-}
-
-function printLetters() {
-    var lettersSpaced = "";
-    for(var i = 0; i < lettersGuessed.length; i++) {
-        lettersSpaced += lettersGuessed[i] + "  ";
-    }
-    lettersSpaced = lettersSpaced.toUpperCase();
-    document.getElementById("letters-guessed").innerHTML = lettersSpaced;
 }
 
 function equals(arr1, arr2) {
-    for(var i = 0; i < arr1.length; i++) {
-        if(arr1[i] != arr2[i]) {
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] != arr2[i]) {
             return false;
         }
     }
     return true;
 }
-function start() {
-    makeWord();
-    numGuesses = 8;
-    lettersGuessed = [];
-    printWins();
-    printWord();
-    printGuesses();
-    printLetters();
-}
 
-function update(guess) {
-    guessLetter(guess);
-    printWins();
-    printWord();
-    printGuesses();
-    printLetters();
-    if(equals(currWord.split(""), wordGuessed)) {
-        alert("You win!");
-        numWins += 1;
-        start();
-    }
-    if(numGuesses === 0) {
-        alert("You Lose!");
-        start();
-    }
-}
-
-start();
-document.addEventListener('keyup', function(e) {
+game.start();
+document.addEventListener('keyup', function (e) {
     guess = e.key;
-    update(guess);
+    game.update(guess);
 });
